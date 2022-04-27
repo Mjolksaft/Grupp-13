6288fe041687c020de5696e961ae16f7c1c8ca8a
Mjolksaft <Kalla01@hotmail.se>
2022-04-28 00:01:54 +0200

    Able to change window

--- src/main.py
+++ src/main.py
@@ -3,15 +3,60 @@ Welcome to the project!.
 
 """
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import QWidget, QApplication
 import sys
-import gui
+
+import DatabaseHandler
+
+class Start(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.dh = DatabaseHandler.DatabaseHandler()
+        uic.loadUi('Start.ui', self)
+        self.setWindowTitle('Start')
+        self.Login.clicked.connect(self.login)
+    
+    def login(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class Login(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.dh = DatabaseHandler.DatabaseHandler()
+        uic.loadUi('Login.ui', self)
+        self.setWindowTitle('Login/register')
+        self.Button.clicked.connect(self.createAccount)
+        self.Button_2.clicked.connect(self.login)
+        self.back.clicked.connect(self.Back)
+
+    def Back(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+    
+    def createAccount(self):
+        """Allows the user to create a account with name and password."""
+        userName = self.Username.text()
+        password = self.Password.text()
+        if userName != "" and password != "":
+            self.dh.createAccount(userName, password)
+
+    def login(self):
+        """Allows the user to login to a specific account."""
+        userName = self.Username_2.text()
+        password = self.Password_2.text()
+        self.dh.login(userName, password)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv) 
 
-    myApp = gui.Start()
-    myApp.show()
+    startScreen = Start()
+    loginScreen = Login()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(startScreen)
+    widget.addWidget(loginScreen)
+    widget.setFixedHeight(500)
+    widget.setFixedWidth(800)
+    widget.show()
     
     try:
         sys.exit(app.exec_())
