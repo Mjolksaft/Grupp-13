09e5c16cbb1105ba591999d2d502e47623bdba76
Mjolksaft <Kalla01@hotmail.se>
2022-05-22 15:09:27 +0200

    kollar på hur man gör test till database

--- src/test_DatabaseHandler.py
+++ src/test_DatabaseHandler.py
@@ -1,21 +1,12 @@
 import unittest
-
 import DatabaseHandler
-from mysql.connector import connect, Error
 
 class TestDatabaseHandler(unittest.TestCase): 
     """DatabaseHandler tests."""
     def test_createAccount(self):
-        dh = DatabaseHandler.DatabaseHandler()
-        # self.assertRaises(Error, dh.createAccount("test","test"))
-
-    def test_login(self):
-        """checks if it returns true if the user test can login with password test"""
-        dh = DatabaseHandler.DatabaseHandler()
-        success = dh.login("test", "test")
-        self.assertTrue(success)
-        failure = dh.login("test", "test2")
-        self.assertFalse(failure)
+        """test so that the user doesnt already exist"""
+        dh = DatabaseHandler.DatabaseHandler
+        self.mock.patch()
 
 if __name__ == '__main__':
     unittest.main()
